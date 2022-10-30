cedcd51d354acd7a392f6cc7c899363d868c4adb
Mohamed Sharaby <moh.sharaby@yahoo.com>
2022-10-30 04:59:35 +0400

    commit

--- resources/views/dashboard/layouts/datatables_scripts.blade.php
+++ resources/views/dashboard/layouts/datatables_scripts.blade.php
@@ -1,43 +1,8 @@
 @push('scripts')
 
-
-    <!-- Datatables-->
-{{--    <script src="{{asset('admin/assets/plugins/datatables/jquery.dataTables.min.js')}}"></script>--}}
-{{--    <script src="{{asset('admin/assets/plugins/datatables/dataTables.bootstrap.js')}}"></script>--}}
-{{--    <script src="{{asset('admin/assets/plugins/datatables/dataTables.buttons.min.js')}}"></script>--}}
-{{--    <script src="{{asset('admin/assets/plugins/datatables/buttons.bootstrap.min.js')}}"></script>--}}
-{{--    <script src="{{asset('admin/assets/plugins/datatables/jszip.min.js')}}"></script>--}}
-{{--    <script src="{{asset('admin/assets/plugins/datatables/pdfmake.min.js')}}"></script>--}}
-{{--    <script src="{{asset('admin/assets/plugins/datatables/vfs_fonts.js')}}"></script>--}}
-{{--    <script src="{{asset('admin/assets/plugins/datatables/buttons.html5.min.js')}}"></script>--}}
-{{--    <script src="{{asset('admin/assets/plugins/datatables/buttons.print.min.js')}}"></script>--}}
-{{--    <script src="{{asset('admin/assets/plugins/datatables/dataTables.fixedHeader.min.js')}}"></script>--}}
-{{--    <script src="{{asset('admin/assets/plugins/datatables/dataTables.keyTable.min.js')}}"></script>--}}
-{{--    <script src="{{asset('admin/assets/plugins/datatables/dataTables.responsive.min.js')}}"></script>--}}
-{{--    <script src="{{asset('admin/assets/plugins/datatables/responsive.bootstrap.min.js')}}"></script>--}}
-{{--    <script src="{{asset('admin/assets/plugins/datatables/dataTables.scroller.min.js')}}"></script>--}}
-
     <script src="{{asset('admin/DataTables/datatables.js')}}"></script>
-
 
     <script src="{{asset('vendor/datatables/buttons.server-side.js')}}"></script>
     {{$dataTable->scripts()}}
-
-
-{{--    <!-- Datatable init js -->--}}
-{{--    <script src="{{asset('admin/assets/pages/datatables.init.js')}}"></script>--}}
-{{--    <script type="text/javascript">--}}
-{{--        $(document).ready(function() {--}}
-{{--            $('#datatable').dataTable();--}}
-{{--            $('#datatable-keytable').DataTable( { keys: true } );--}}
-{{--            $('#datatable-responsive').DataTable();--}}
-{{--            $('#datatable-scroller').DataTable( { ajax: "admin/assets/plugins/datatables/json/scroller-demo.json", deferRender: true, scrollY: 380, scrollCollapse: true, scroller: true } );--}}
-{{--            var table = $('#datatable-fixed-header').DataTable( { fixedHeader: true } );--}}
-{{--        } );--}}
-{{--        TableManageButtons.init();--}}
-
-{{--    </script>--}}
-
-
 
 @endpush
--- resources/views/dashboard/layouts/styles.blade.php
+++ resources/views/dashboard/layouts/styles.blade.php
@@ -25,10 +25,3 @@
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/fancyapps/fancybox@3.5.7/dist/jquery.fancybox.min.css" />
 
 
-{{--<!-- DataTables -->--}}
-{{--<link href="{{asset('admin/assets/plugins/datatables/jquery.dataTables.min.css')}}" rel="stylesheet" type="text/css" />--}}
-{{--<link href="{{asset('admin/assets/plugins/datatables/buttons.bootstrap.min.css')}}" rel="stylesheet" type="text/css" />--}}
-{{--<link href="{{asset('admin/assets/plugins/datatables/fixedHeader.bootstrap.min.css')}}" rel="stylesheet" type="text/css" />--}}
-{{--<link href="{{asset('admin/assets/plugins/datatables/responsive.bootstrap.min.css')}}" rel="stylesheet" type="text/css" />--}}
-{{--<link href="{{asset('admin/assets/plugins/datatables/scroller.bootstrap.min.css')}}" rel="stylesheet" type="text/css" />--}}
-
